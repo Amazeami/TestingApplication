2b9778bafd44fbb215b1d10964d695d10ffed53a
Amit Raj <ami0275@gmail.com>
2020-08-09 08:53:11 +0530

    Depedency for ActivityTestRule added.

--- app/src/androidTest/java/com/example/myapplication/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/myapplication/MainActivityTest.kt
@@ -1,11 +1,22 @@
 package com.example.myapplication
 
+import androidx.test.filters.LargeTest
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
 import org.junit.After
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.runner.RunWith
 
+
+@LargeTest
 class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
 
     @Before
     fun setUp() {
